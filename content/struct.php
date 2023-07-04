3d7b19a386c5f54561b5ac6cd5c1fff45a93a743
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2023-07-04 09:05:26 +0300

    Create struct.php

---
+++ content/struct.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html class="html" >
+
+    <head>
+
+        <?php 
+
+            require("meta.php");
+
+        ?>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K2K1GGYL0B"></script>
+
+        <script>
+
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){dataLayer.push(arguments);}
+
+            gtag('js', new Date());
+
+
+
+            gtag('config', 'G-K2K1GGYL0B');
+
+        </script>
+
+        <style>
+
+        </style>
+
+    </head>    
+
+    <body>
+
+    </body>
+
+</html>
